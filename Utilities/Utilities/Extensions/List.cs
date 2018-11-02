415adffdab97454b0c79e50d640aa608f60b48f1
Arthur Clark <arthur.clark@oit.edu>
2018-11-02 10:55:45 -0700

    Initial Commit

---
+++ Utilities/Utilities/Extensions/List.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilities.Extensions
+{
+    public static class List
+    {
+        public static bool Enqueue<T>(this List<T> list, T value)
+        {
+            try
+            {
+                list.Add(value);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static T Dequeue<T>(this List<T> list)
+        {
+            if (!list.Any()) throw new NullReferenceException("List is Empty!");
+            var retVal = list.First();
+            list.Remove(list.First());
+            return retVal;
+        }
+
+        public static T Peek<T>(this List<T> list)
+        {
+            if (list.Any())
+            {
+                return list.First();
+            }
+            throw new NullReferenceException("List is Empty!");
+        }
+    }
+}
